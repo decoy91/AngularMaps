9955f05295c8394a0772ffd76194ab953282dc86
Aleks <gajim91@gmail.com>
2024-05-20 15:24:51 -0600

    Diseño del ZoomIn y ZoomOut en la pantalla del html

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit{
+
+  @ViewChild('map') divMap?: ElementRef;
+
+
+
+  ngAfterViewInit(): void {
+
+    if( !this.divMap ) throw 'Elemento no encontrado'
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-93.12,16.75], // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+  }
+
 
 }
