781b3f560ba5d051f0d829e8dbfd96192d496a46
Aleks <gajim91@gmail.com>
2024-05-21 14:57:22 -0600

    Controlar el nivel del zoom y acciones de los botones

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,19 +9,50 @@ export class ZoomRangePageComponent implements AfterViewInit{
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number = 10;
 
+  public map?: Map;
 
   ngAfterViewInit(): void {
 
     if( !this.divMap ) throw 'Elemento no encontrado'
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-93.12,16.75], // starting position [lng, lat]
-      zoom: 10, // starting zoom
+      zoom: this.zoom, // starting zoom
+    });
+
+    this.mapListeners();
+
+  }
+
+  mapListeners(){
+    if( !this.map) throw 'Mapa sin inicializar';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
     });
   }
 
+  zoomIn(){
+
+    this.map?.zoomIn();
+
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+  zoomChanged(value: string){
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 
 }
