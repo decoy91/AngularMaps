85f190312d73f55a569ab55e0c7ac34d71a9d534
Aleks <gajim91@gmail.com>
2024-05-22 15:07:20 -0600

    Añadiendo marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from "mapbox-gl";
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-93.11520332908265, 16.752918789579596);
+
+  ngAfterViewInit(): void {
+
+    if( !this.divMap ) throw 'Elemento no encontrado'
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+    });
+
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Alex H.';
+    // const marker = new Marker(
+    //   {
+    //     //color:'red',
+    //     element: markerHtml
+    //   }
+    // )
+    // .setLngLat(this.currentLngLat)
+    // .addTo(this.map);
+
+  }
 
 }
