6c03eac8f502b2489343b1469cb1f8b684039ed6
Aleks <gajim91@gmail.com>
2024-05-23 14:18:21 -0600

    Añadiendo marcadores de forma dinamica

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from "mapbox-gl";
+import { subscriptionLogsToBeFn } from 'rxjs/internal/testing/TestScheduler';
 
 @Component({
   templateUrl: './markers-page.component.html',
@@ -35,6 +36,24 @@ export class MarkersPageComponent {
     // .setLngLat(this.currentLngLat)
     // .addTo(this.map);
 
+  }
+
+  createMarker(){
+    if ( !this.map )  return;
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+
+
+  addMarker(lngLat:LngLat, color: string){
+    if(!this.map) return;
+    const marker = new Marker({
+      color:color,
+      draggable: true
+    })
+    .setLngLat(lngLat)
+    .addTo( this.map );
   }
 
 }
