7a622d2227decfbdbaa47a615b26960b14fde0d4
Aleks <gajim91@gmail.com>
2024-06-11 22:10:39 -0600

    standalone como componente reutilizable

--- src/app/alone/pages/alone-pages/alone-pages.component.ts
+++ src/app/alone/pages/alone-pages/alone-pages.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
+  imports: [ CounterAloneComponent, ],
   templateUrl: './alone-pages.component.html',
   styleUrl: './alone-pages.component.css'
 })
